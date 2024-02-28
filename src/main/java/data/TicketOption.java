c671ea0534639463f66c83a6f9456d0554008f0d
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2024-02-29 00:54:52 +0800

    Update TicketOption.java

--- src/main/java/data/TicketOption.java
+++ src/main/java/data/TicketOption.java
@@ -1,5 +1,20 @@
 package data;
 
+import java.util.List;
+import java.util.Map;
+
 public class TicketOption {
-    
+    private Map<String, float> options;
+
+    public TicketOption(Map<String, float> options) {
+        this.options = options;
+    }
+
+    public List<String> getOptions() {
+        return null;
+    }
+
+    public float getPrice() {
+        return null;
+    }
 }
