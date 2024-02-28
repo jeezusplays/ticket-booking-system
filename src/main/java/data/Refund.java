8ef9fc0ff6106cde390ab2877d74b9e80dd228d4
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2024-02-29 00:54:50 +0800

    Update Refund.java

--- src/main/java/data/Refund.java
+++ src/main/java/data/Refund.java
@@ -1,5 +1,58 @@
 package data;
 
+import java.util.Map;
+
 public class Refund {
-    
+    private int refundId;
+    private int bookingId;
+    private double refundAmount;
+    private String refundDate;
+    private String refundStatus;
+
+    public Refund(int refundId, int bookingId, double refundAmount, String refundDate, String refundStatus) {
+        this.refundId = refundId;
+        this.bookingId = bookingId;
+        this.refundAmount = refundAmount;
+        this.refundDate = refundDate;
+        this.refundStatus = refundStatus;
+    }
+
+    // Getters
+    public int getRefundId() {
+        return refundId;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public double getRefundAmount() {
+        return refundAmount;
+    }
+
+    public String getRefundDate() {
+        return refundDate;
+    }
+
+    public String getRefundStatus() {
+        return refundStatus;
+    }
+
+    // Setters
+    public void setRefundAmount(double refundAmount) {
+        this.refundAmount = refundAmount;
+    }
+
+    public void setRefundDate(String refundDate) {
+        this.refundDate = refundDate;
+    }
+
+    public void setRefundStatus(String refundStatus) {
+        this.refundStatus = refundStatus;
+    }
+
+    // Other methods
+    public Map<String, float> getRefundDetails() {
+        return null;
+    }
 }
