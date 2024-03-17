5ed214ac30b604432f4a0419111fccd407b40e72
chnpnd <nelsonho.jsccyn@gmail.com>
2024-03-18 01:46:36 +0800

    Updated: + Reshifted constructor method and added String name [Customer.java, EventManager.java, TicketingOfficer.java, User.java] + Replaced username to email and added getter for name [User.java] + Imported necessary package [EventManager.java, TicketingOfficer.java] + Added another constructor for new EventManager and TicketingOfficer user with no managedEvents and ticketingEvents [EventManager.java, TicketingOfficer.java]

--- src/main/java/user/TicketingOfficer.java
+++ src/main/java/user/TicketingOfficer.java
@@ -1,13 +1,20 @@
 package user;
 
+import data.Event;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TicketingOfficer extends User {
     private List<Event> ticketingEvents;
 
-    public TicketingOfficer(int userID, String username, String password, String email, String type, List<Event> ticketingEvents) {
-        super(userID, username, password, email, type);
+    public TicketingOfficer(int id, String email, String password, String name, String type, List<Event> ticketingEvents) {
+        super(id, email, password, name, type);
         this.ticketingEvents = ticketingEvents;
+    }
+
+    public TicketingOfficer(int id, String email, String password, String name, String type) {
+        super(id, email, password, name, type);
+        this.ticketingEvents = new ArrayList<>();
     }
 
     public List<Event> getTicketingEvents() {
